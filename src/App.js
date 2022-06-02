1bf64793fafb5d01e35e486df098dbb15a623755
Ogaga <iyaraogaga69@gmail.com>
2022-06-02 09:30:02 +0100

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-
+    <>
+      <div className="navbar">
+        <h1 className="header-one">Bookstore</h1>
+        <nav>
+          <Link className="navlink" to="/">Book</Link>
+          <Link className="navlink" to="/Category">Category</Link>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Book />} />
+        <Route path="/Category" element={<Category />} />
+      </Routes>
+    </>
   );
 }
 
